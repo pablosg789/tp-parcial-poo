1c92aa72d51acdab6a50abf0e4d53ae32c08d0a9
pablosg789 <42720199+pablosg789@users.noreply.github.com>
2018-11-29 20:13:23 -0300

    Add files via upload

--- POO/app.py
+++ POO/app.py
@@ -57,7 +57,6 @@ def ingresar():
                 if formulario.usuario.data == registro[0] and formulario.password.data == registro[1]:
                     flash('Bienvenido')
                     session['username'] = formulario.usuario.data
-
                 # Ingreso de usuario
 
                 # Ingresar a TABLA DE VENTAS
@@ -75,24 +74,7 @@ def ingresar():
                             cantidad.append(lista[3])
                             precio.append(lista[4])
                         rango = len(codigo)
-                    # creamos un diccionario por producto y cantidad
-                        producto_cantidad = Utilidad.CrearDiccionario(producto,cantidad)
-                        producto_cantidad = Utilidad.OrdenarDiccionario(producto_cantidad)
-                        producto_cantidad.reverse()
-                        print(producto_cantidad)
-                    # creamos un diccionario por cliente y precio
-                        gasto = Utilidad.GastoCliente(cantidad,precio)
-                        print(gasto)
-
-                        cliente_gasto = Utilidad.CrearDiccionario(cliente[1:],gasto)
-                        cliente_gasto = Utilidad.OrdenarDiccionario(cliente_gasto)
-                        cliente_gasto.reverse()
-                        gasto.insert(0,'GASTO')
-                        print(cliente_gasto)
-
-
-                    # Validacion de campos
-
+                        # Validacion de campos
                         if Validacion.ValidarCampoCodigo(codigo[1:]) == False:
                             return render_template('no_valido.html' , mensaje = 'El campo CODIGO debe estar compuesto por 3 letras máyusculas seguidas de 3 caracteres númericos')
                         elif Validacion.ValidarCampoCantidad(cantidad[1:]) == False:
@@ -100,73 +82,93 @@ def ingresar():
                         elif Validacion.ValidarCampoPrecio(precio[1:]) == False:
                             return render_template('no_valido.html' , mensaje = 'El campo PRECIO solo premite números decimales')
                         elif Validacion.ValidarCampos(archivo_csv) == False:
-                            return render_template('no_valido.html' , mensaje= 'Verifique la cantidad de CAMPO')
+                            return render_template('no_valido.html' , mensaje= 'Verifique la cantidad de CAMPOS ingresados  en su archivo CSV')
                         else:
                             return render_template('tabla.html',nombre= 'Tabla de Ventas',nameParametro = 'TABLA', campo1 = codigo, campo2 = producto,campo3 = cliente,
-                            campo4 = cantidad, campo5 = precio, rango= rango, producto_cantidad= producto_cantidad, cliente_gasto = cliente_gasto)
+                            campo4 = cantidad, campo5 = precio, rango= rango)
                     else:
                         return render_template('no_valido.html', mensaje= 'El csv archivo inexistente')
-
                 registro = next(usuarios_csv, None)
             else:
                 flash('Revisá nombre de usuario y contraseña')
                 return redirect(url_for('ingresar'))
     return render_template('login.html', formulario=formulario)
 
-@app.route('/consulta', methods=["GET", 'POST'])
-def consulta():
+@app.route('/compra_cliente', methods=["GET", 'POST'])
+def compra_cliente():
     if request.method == 'POST':
         nombre_cliente = request.form['nombre']
         apellido_cliente = request.form['apellido']
         nombre_completo = Utilidad.CombinarNombre(nombre_cliente,apellido_cliente)
-        print(nombre_completo)
-
         if nombre_completo == False:
-            return render_template('no_valido.html', mensaje= 'Erro al ingresar los datos del cliente')
+            return render_template('no_valido.html', mensaje= 'Error al ingresar los datos del cliente')
+        archivo_csv = Archivo.AbrirComoLista('tabla.csv')
+# crea un diccionario que contiene en listas el contenido de los 5 campos
+        dic_campos = Archivo.DicCampos(archivo_csv)
+        if nombre_completo in dic_campos['cliente']:
+# se crea un diccionario con 2 que contiene 2 listas una con los nombres de los productos y otra con los codigos
+            cliente_1 = Utilidad.CompradoPorCliente(nombre_completo,dic_campos['cliente'],dic_campos['codigo'],dic_campos['producto'])
+            return render_template('compra_cliente.html', nombre_completo = nombre_completo ,codigo = cliente_1['codigo']
+            ,producto = cliente_1['producto'] ,rango = len(cliente_1['codigo']))
+        else:
+            return render_template('no_valido.html', mensaje='No se registran clientes con el nombre {}'.format(nombre_completo))
+    return render_template('sin_permiso.html')
+
+@app.route('/clientes_por_producto', methods=["GET", 'POST'])
+def clientes_por_producto():
+    if request.method == 'POST':
         codigo_producto = request.form['codigo']
         codigo_producto = codigo_producto.upper()
-
-
         if codigo_producto == False:
             return render_template('no_valido.html', mensaje= 'Error en el código del producto')
         archivo_csv = Archivo.AbrirComoLista('tabla.csv')
-        cliente = []
-        codigo = []
-        producto = []
-        cantidad = []
-        precio = []
-        for lista in archivo_csv:
-            codigo.append(lista[0])
-            producto.append(lista[1])
-            cliente.append(lista[2])
-            cantidad.append(lista[3])
-            precio.append(lista[4])
-            
-    # obtener indices de listas
-        indices_cliente = Utilidad.CapturarIndices(nombre_completo,cliente)
-        
-    # creamos un diccionario con los productos que compro un cliente
-        dicc_cliente = Utilidad.ClienteCompras(nombre_completo,indices_cliente,producto)
-        productos_comprados = dicc_cliente[nombre_completo] 
-        
+# crea un diccionario que contiene en listas el contenido de los 5 campos
+        dic_campos = Archivo.DicCampos(archivo_csv)
+        if codigo_producto in dic_campos['codigo']:
+# se crea un diccionario con 1 string con el nombre del producto y una lista con los nombres de los clientes
+            dicc = Utilidad.ClientesPorProducto(codigo_producto,dic_campos['codigo'],dic_campos['cliente'],dic_campos['producto'])
+            nombre_producto = dicc['producto']
+            lista_clientes = dicc['clientes']
+            return render_template('clientes_por_producto.html', codigo_producto = codigo_producto , nombre_producto = nombre_producto
+            ,lista_clientes = lista_clientes ,rango = len(lista_clientes))
+        else:
+            return render_template('no_valido.html', mensaje='El código {} no se encuentra registrado'.format(codigo_producto))
+    return render_template('sin_permiso.html')
 
-    # obtener indices codigos
-        indices_codigo = Utilidad.CapturarIndices(codigo_producto,codigo)
-        
-    # creamos un diccionario con los clientes que compraron un producto
-        dicc_producto = Utilidad.ProductoClientes(codigo_producto,indices_codigo,cliente)
-        clientes_que_compraron = dicc_producto[codigo_producto]
-        i = codigo.index(codigo_producto)
-        
-        return render_template('consulta.html',nombre='Consulta', productos_por_cliente = productos_comprados,
-         nombre_completo = nombre_completo,clientes_por_producto=clientes_que_compraron ,codigo_producto = codigo_producto,
-         nombre_producto = producto[i])
+@app.route('/mejores_clientes',methods=["GET", 'POST'])
+def mejores_clientes():
+    if request.method == 'POST':
+        archivo_csv = Archivo.AbrirComoLista('tabla.csv')
+    # crea un diccionario que contiene en listas el contenido de los 5 campos
+        dic_campos = Archivo.DicCampos(archivo_csv)
+    # se crea un diccionario con 1 string con el nombre del producto y una lista con los nombres de los clientes
+        cliente_gasto = Utilidad.TopClientes(dic_campos['cliente'],archivo_csv)
+    # se ordenan las listas de mayor a menor segun los gastos de los clientes
+        dic_cliente_gasto = Utilidad.OrdenarListasClienteGastos(cliente_gasto[0],cliente_gasto[1])
+        rango = len(cliente_gasto[0])
+        return render_template('mejores_clientes.html',lista_clientes = dic_cliente_gasto['cliente'], lista_gastos = dic_cliente_gasto['gasto'], rango = rango)
+    else:
+        return render_template('sin_permiso.html')
 
-    return redirect(url_for('ingresar'))
+@app.route('/productos_mas_vendidos',methods=["GET", 'POST'])
+def productos_mas_vendidos():
+    if request.method == 'POST':
+        archivo_csv = Archivo.AbrirComoLista('tabla.csv')
+    # crea un diccionario que contiene en listas el contenido de los 5 campos
+        dic_campos = Archivo.DicCampos(archivo_csv)
+    # se crea un diccionario con 1 string con el nombre del producto y una lista con los nombres de los clientes
+        producto_cantidad = Utilidad.TopProductos(dic_campos['producto'],archivo_csv)
+    # se ordenan las listas de mayor a menor segun la cantidad de ventas
+        dic_prod_cantidad = Utilidad.OrdenarListasProductosCantidad(producto_cantidad[0],producto_cantidad[1])
+        rango = len(producto_cantidad[0])
+        return render_template('productos_mas_vendidos.html',lista_productos = dic_prod_cantidad['producto'],
+         lista_cantidad = dic_prod_cantidad['cantidad'], rango = rango)
+    else:
+        return render_template('sin_permiso.html')
 
 
 
-@app.route('/registrar')
+@app.route('/registrar', methods=['GET', 'POST'])
 def registrar():
     formulario = RegistrarForm()
     if formulario.validate_on_submit():
@@ -180,10 +182,6 @@ def registrar():
         else:
             flash('Las passwords no matchean')
     return render_template('registrar.html', form=formulario)
-
-#app.route("/prueba")
-#def prueba():
-
 
 
 @app.route('/secret', methods=['GET'])
--- POO/mis_clases.py
+++ POO/mis_clases.py
@@ -1,7 +1,8 @@
 import operator
 import csv
+import string
 class Validacion():
-    
+
 
     def ValidarArchivo(archivo):
         try:
@@ -13,8 +14,8 @@ class Validacion():
 
     def ValidarCampos(lista):
         try:
-            for i in range(len(lista)-1):
-                if len(lista[i]) == 6:
+            for i in lista:
+                if len(i) == 5:
                     condicion = True
                 else:
                     condicion = False
@@ -23,18 +24,17 @@ class Validacion():
             return condicion
         except:
             return False
-        
-    
+
     def ValidarCampoCodigo(lista):
         try:
-            for valor in lista: 
+            for valor in lista:
                 if valor[0:3].isupper() and valor[0:3].isalpha() and valor.isalnum() and valor[3:6].isdigit() and len(valor) == 6:
-                    condicion = True            
+                    condicion = True
                 else:
                     condicion = False
                     if condicion == False:
                         break
-            return condicion 
+            return condicion
         except:
             return False
 
@@ -44,18 +44,18 @@ class Validacion():
                     if type(valor) == str:
                         numero = eval(valor)
                         if numero % 1 == 0:
-                            condicion = True            
+                            condicion = True
                         else:
                             condicion = False
                             if condicion == False:
                                 break
                     elif valor % 1 == 0:
                         condicion = True
-                    else:                
+                    else:
                         condicion = False
                         if condicion == False:
-                            break   
-                return condicion 
+                            break
+                return condicion
             except:
                 return False
 
@@ -63,12 +63,12 @@ class Validacion():
         try:
             for valor in lista:
                 if float(valor) % 1 != 0:
-                    condicion = True            
+                    condicion = True
                 else:
                     condicion = False
                     if condicion == False:
                         break
-            return condicion 
+            return condicion
         except:
             return False
 
@@ -76,84 +76,37 @@ class Archivo():
 
     def AbrirComoLista(archivo):
         with open(archivo) as csv_file:
-            leer_archivo = csv.reader(csv_file, delimiter = ';')
+            leer_archivo = csv.reader(csv_file)
             campos = []
             for campo_lista in leer_archivo:
                 campos.append(campo_lista)
             return campos
+    def DicCampos(campos):
+        cliente = []
+        codigo = []
+        producto = []
+        cantidad = []
+        precio = []
+        for lista in campos:
+            codigo.append(lista[0])
+            producto.append(lista[1])
+            cliente.append(lista[2])
+            cantidad.append(lista[3])
+            precio.append(lista[4])
+        dic_campos = {}
+        dic_campos['cliente']= cliente
+        dic_campos['codigo']= codigo
+        dic_campos['producto']= producto
+        dic_campos['cantidad']= cantidad
+        dic_campos['precio']= precio
+        return  dic_campos
 
 class Utilidad():
+
     def CombinarNombre(nombre,apellido): # combina nombre y apellido capitalizados
         try:
-            nombre_completo = nombre.capitalize() +' '+ apellido.capitalize()
+            nombre_completo = nombre.strip().capitalize() +' '+ apellido.strip().capitalize()
             return nombre_completo
-        except:
-            return False
-  
-    def CapturarIndices(nombre,lista): # guarda los indices en una lista en los que aparece nombre
-        try:
-            consulta = nombre
-            indice= []
-            for contador, nombre in enumerate(lista):
-                if consulta == nombre:
-                    indice.append(contador)        
-            return indice
-        except:
-            return False
-    
-    def IndicesPorCampos(indice,campo): # guarda en una lista los valores indicados por indice
-        try:                            # del campo ingresado               
-            lista = []
-            for index in indice:
-                lista.append(campo[index])
-            return lista
-        except:
-            False   
-
-    def ClienteCompras(nombre,indice,producto):
-        try:
-            cliente = {}
-            compra = []
-            for i in indice:
-                compra.append(producto[i])
-            cliente[nombre] = compra
-            return cliente
-        except:
-            return False
-    def ProductoClientes(codigo,indice,cliente):
-        try:
-            producto = {}
-            compra = []
-            for i in indice:
-                compra.append(cliente[i])
-            producto[codigo] = compra
-            return producto
-        except:
-            return False
-
-    def SumarCantidad(lista): # suma la cantidad de una lista producto determinado
-
-            try:
-                num = 0
-                for valor in lista:
-                    num = num + int(valor)
-                return num
-            except:
-                return False
-    def SumarPrecios(lista): # suma los precios de un cliente determinado
-            try:
-                num = 0.0
-                for valor in lista:
-                    num = num + float(valor)
-                return num
-            except:
-                return False
-    def CrearDiccionario(campo1,campo2):
-        try:
-            dicc = {}
-            for i in range(len(campo1)):
-                dicc[campo1[i]] = campo2[i] 
-            return dicc
         except:
             return False
 
@@ -161,24 +114,120 @@ class Utilidad():
         try:
             diccionario = sorted(diccionario.items(),key=operator.itemgetter(1))
             return diccionario
-            
+
         except:
             return False
-    def GastoCliente(cantidad,precio):
+
+    def CompradoPorCliente(nombre,cliente,codigo,producto):
         try:
-            gasto = []
-            for i in range(1,len(cantidad)):
-                gasto.append(float(cantidad[i]) * float(precio[i]))
-            return gasto
+            indice= []
+            for contador, consulta in enumerate(cliente):
+                if consulta == nombre:
+                    indice.append(contador)
+            cod = []
+            pro = []
+            for i in indice:
+                cod.append(codigo[i])
+                pro.append(producto[i])
+            productos_comprados_por_cliente = {}
+            productos_comprados_por_cliente['producto'] = pro
+            productos_comprados_por_cliente['codigo']= cod
+            return productos_comprados_por_cliente
+
         except:
             return False
 
-# verificar que un elemento de la lista CLIENTE este en la lista y conseguir los indices
-# mostrar todos los PRODUCTO con los mismos indices
+    def ClientesPorProducto(num_cod,codigo,cliente,producto):
+        try:
+            indice= []
+            for contador, consulta in enumerate(codigo):
+                if consulta == num_cod:
+                    indice.append(contador)
+            nombre_producto = producto[indice[0]]
+            nombres = []
+            for i in indice:
+                nombres.append(cliente[i])
+            producto_con_lista_clientes = {}
+            producto_con_lista_clientes['clientes']= nombres
+            producto_con_lista_clientes['producto']= nombre_producto
+            return producto_con_lista_clientes
+        except:
+            return False
 
-# verificar que un elemento de la lista CODIGO este en la lista y conseguir los indices
-# veridicar los indices con los de clientes y mostrarlos
+    def TopClientes(cliente,archivo_csv):
+        try:
+            list_nombre = []
+            list_gasto = []
+            for nombre in cliente: # para nombre el nombre de la lista cliente
+                if nombre in list_nombre:
+                    continue
+                for i in archivo_csv[1:]: # para i la lista de campos
 
+                    if nombre in i: # si nombre esta en i(la lista de campos)
+                        if nombre in list_nombre: # si nombre(uno de la lista de clientes) esta en list_nombre
+                            indice = list_nombre.index(nombre) # indice guarda el numero de posicion
+                            list_gasto[indice] = list_gasto[indice] + float(i[3]) * float(i[4]) # cambia el valor que ya tenia list_gasto
 
+                        else: # si no esta nombre en i (la lista de campos)
+                            list_nombre.append(nombre) # se debe agregar nombre a list_nombre
+                            list_gasto.append(float(i[3]) * float(i[4])) # se debe agregar los gastos a list_gasto
+            cliente_gasto = [list_nombre,list_gasto]
+            return cliente_gasto
+        except:
+            return False
 
+    def TopProductos(prducto,archivo_csv):
+        try:
+            list_producto = []
+            list_cantidad = []
+            for nombre in prducto: # para nombre el nombre de la lista cliente
+                if nombre in list_producto:
+                    continue
+                for i in archivo_csv[1:]: # para i la lista de campos
+                    if nombre in i: # si nombre esta en i(la lista de campos)
+                        if nombre in list_producto: # si nombre(uno de la lista de clientes) esta en list_producto
+                            indice = list_producto.index(nombre) # indice guarda el numero de posicion
+                            list_cantidad[indice] = list_cantidad[indice] + int(i[3]) # cambia el valor que ya tenia list_cantidad
+                        else: # si no esta nombre en i (la lista de campos)
+                            list_producto.append(nombre) # se debe agregar nombre a list_producto
+                            list_cantidad.append(int(i[3])) # se debe agregar los gastos a list_cantidad
+            producto_cantidad = [list_producto,list_cantidad]
+            return producto_cantidad
+        except:
+            return False
 
+    def OrdenarListasProductosCantidad(producto,cantidad):
+        try:
+            for i in range(len(cantidad)):
+                for x in range(len(cantidad)-1):
+                    if int(cantidad[x]) < int(cantidad[x+1]):
+                        aux1= int(cantidad[x])
+                        cantidad[x] = int(cantidad[x+1])
+                        cantidad[x+1]= aux1
+                        aux2=producto[x]
+                        producto[x]=producto[x+1]
+                        producto[x+1]=aux2
+            dic = {}
+            dic['producto'] = producto
+            dic['cantidad'] = cantidad
+            return dic
+        except:
+            return False
+
+    def OrdenarListasClienteGastos(cliente,gastos):
+        try:
+            for i in range(len(cliente)):
+                for x in range(len(cliente)-1):
+                    if float(gastos[x]) < float(gastos[x+1]):
+                        aux1= float(gastos[x])
+                        gastos[x] = float(gastos[x+1])
+                        gastos[x+1]= aux1
+                        aux2=cliente[x]
+                        cliente[x]=cliente[x+1]
+                        cliente[x+1]=aux2
+            dic = {}
+            dic['cliente'] = cliente
+            dic['gasto'] = gastos
+            return dic
+        except:
+            return False
